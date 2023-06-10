b7832649482a61353b4e6f9cb7bf26ce827071d9
zhruith <zhruith@gmail.com>
2023-06-10 14:02:51 +0800

    store my nvim configurations

---
+++ conf.lua
@@ -0,0 +1,595 @@
+local icons = require('icons')
+local use_icons = true
+---------------- ayutheme config ----------------
+
+-- :lua print(vim.inspect(require('ayu.colors')))
+local ayu = require 'ayu.colors'
+local line = "#273747"
+local gray = { fg = 'gray' }
+
+require('ayu').setup {
+  overrides = function()
+    return {
+      Normal = { bg = nil },
+      --TabLineFill = { bg = nil },
+      --FloatBorder = {bg='#ff00ff',fg='white'},
+      NormalFloat = { bg = line },
+      SignColumn = { bg = nil },
+      VertSplit = { fg = ayu.ui },
+      Visual = { bg = ayu.selection_border },
+      CursorLine = { bg = line },
+      CursorLineNr = { bg = line },
+      Search = { bg = ayu.ui },
+      Comment = gray,
+      LineNr = { fg = ayu.comment },
+      CocHighlightText = { bg = '#505050' },
+      NonText = gray,
+      DiagnosticHint = gray,
+      SpecialKey = gray,
+      NvimTreeIndentMarker = gray,
+    }
+  end,
+}
+
+require 'ayu'.colorscheme()
+
+---------------- gitsigns config ------------------
+
+require('gitsigns').setup {
+  signs = {
+    add = {
+      text = icons.ui.LineLeft,
+      hl = "GitSignsAdd",
+      numhl = "GitSignsAddNr",
+      linehl = "GitSignsAddLn",
+    },
+    change = {
+      text = icons.ui.LineLeft,
+      hl = "GitSignsChange",
+      numhl = "GitSignsChangeNr",
+      linehl = "GitSignsChangeLn",
+    },
+    delete = {
+      text = icons.ui.Triangle,
+      hl = "GitSignsDelete",
+      numhl = "GitSignsDeleteNr",
+      linehl = "GitSignsDeleteLn",
+    },
+    topdelete = {
+      text = icons.ui.Triangle,
+      hl = "GitSignsDelete",
+      numhl = "GitSignsDeleteNr",
+      linehl = "GitSignsDeleteLn",
+    },
+    changedelete = {
+      text = icons.ui.LineLeft,
+      hl = "GitSignsChange",
+      numhl = "GitSignsChangeNr",
+      linehl = "GitSignsChangeLn",
+    },
+  },
+  signcolumn = true,
+  numhl = false,
+  linehl = false,
+  word_diff = false,
+  watch_gitdir = {
+    interval = 1000,
+    follow_files = true,
+  },
+  attach_to_untracked = true,
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+    delay = 100,
+    ignore_whitespace = false,
+  },
+  current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+  sign_priority = 6,
+  status_formatter = nil, -- Use default
+  update_debounce = 200,
+  max_file_length = 40000,
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = "rounded",
+    style = "minimal",
+    relative = "cursor",
+    row = 0,
+    col = 1,
+  },
+  yadm = { enable = false },
+
+}
+
+-------------- lualine config ----------------
+
+local llayu = require 'lualine.themes.ayu'
+llayu.normal.c = { fg = 'darkgray', bg = nil }
+
+require('lualine').setup {
+  options = {
+    theme = llayu,
+    globalstatus = true,
+    component_separators = '·',
+    --component_separators = { right = icons.ui.TriangleShortArrowLeft, left = icons.ui.TriangleShortArrowRight },
+    section_separators = {
+      left = icons.ui.SemicircleRight,
+      right = icons.ui.SemicircleLeft
+    },
+    disabled_filetypes = {
+      statusline = { '', 'list' },
+      winbar = {}
+    },
+  },
+  sections = {
+    lualine_a = {
+      {
+        function() return ' ' .. icons.misc.Vimlogo2 end,
+        padding = 0,
+      },
+    },
+    lualine_b = {
+      'branch',
+    },
+    lualine_c = {
+      'filename',
+      {
+        'diff',
+        colored = true,
+        diff_color = {
+          added = { fg = ayu.vcs_added },
+          modified = { fg = ayu.vcs_modified },
+          removed = { fg = ayu.vcs_removed },
+        },
+        symbols = {
+          added = icons.git.LineAdded .. ' ',
+          modified = icons.git.LineRemoved .. ' ',
+          removed = icons.git.LineRemoved .. ' ',
+        },
+        source = function()
+          local gitsigns = vim.b.gitsigns_status_dict
+          if gitsigns then
+            return {
+              added = gitsigns.added,
+              modified = gitsigns.changed,
+              removed = gitsigns.removed,
+            }
+          end
+        end,
+      },
+      'g:coc_status',
+    },
+    lualine_x = {
+      {
+        'diagnostics',
+        symbols = {
+          error = icons.diagnostics.BoldError .. ' ',
+          warn = icons.diagnostics.BoldWarning .. ' ',
+          info = icons.diagnostics.BoldInformation .. ' ',
+          hint = icons.diagnostics.BoldHint .. ' ',
+        },
+      },
+      'fileformat',
+      'filetype',
+      { 'datetime', style = '%H:%M' } },
+    lualine_y = {
+      'progress',
+      {
+        'progress',
+        fmt = function() return "%L" end,
+      },
+    },
+    lualine_z = { 'location' },
+  },
+}
+---------------- toggleterm config ------------------
+
+require('toggleterm').setup {
+  persist_mode = false,
+  persist_size = true,
+  size = function(term)
+    if term.direction == 'horizontal' then
+      return 15
+    elseif term.direction == 'vertical' then
+      return vim.o.columns * 0.4
+    else
+      return 20
+    end
+  end,
+}
+
+-------------- bufferline config --------------------
+
+local function is_ft(b, ft)
+  return vim.bo[b].filetype == ft
+end
+
+local function diagnostics_indicator(num, _, diagnostics, _)
+  local result = {}
+  local symbols = {
+    error = icons.diagnostics.BoldError,
+    warning = icons.diagnostics.BoldWarning,
+    info = icons.diagnostics.BoldInformation,
+  }
+  if not use_icons then
+    return "(" .. num .. ")"
+  end
+  for name, count in pairs(diagnostics) do
+    if symbols[name] and count > 0 then
+      table.insert(result, symbols[name] .. "" .. count)
+    end
+  end
+  --result = table.concat(result, " ")
+  return #result > 0 and result or ""
+end
+
+local function custom_filter(buf, buf_nums)
+  local logs = vim.tbl_filter(function(b)
+    return is_ft(b, "log")
+  end, buf_nums or {})
+  if vim.tbl_isempty(logs) then
+    return true
+  end
+  local tab_num = vim.fn.tabpagenr()
+  local last_tab = vim.fn.tabpagenr "$"
+  local is_log = is_ft(buf, "log")
+  if last_tab == 1 then
+    return true
+  end
+  -- only show log buffers in secondary tabs
+  return (tab_num == last_tab and is_log) or (tab_num ~= last_tab and not is_log)
+end
+
+local bufferline = require('bufferline')
+bufferline.setup {
+  options = {
+    style_preset = {
+      bufferline.style_preset.no_italic,
+      bufferline.style_preset.nobold,
+    },
+    always_show_bufferline = false,
+    offsets = { { filetype = 'NvimTree' } },
+    diagnostics = 'coc',
+    diagnostics_update_in_insert = false,
+    diagnostics_indicator = diagnostics_indicator,
+    custom_filter = custom_filter,
+  },
+}
+
+---------------- nvim-tree config ----------------
+
+
+local function on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return {
+      desc = "nvim-tree: " .. desc,
+      buffer = bufnr,
+      noremap = true,
+      silent = true,
+      nowait = true
+    }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  local useful_keys = {
+    ["l"] = { api.node.open.edit, opts "Open" },
+    ["o"] = { api.node.open.edit, opts "Open" },
+    ["h"] = { api.node.navigate.parent_close, opts "Close Directory" },
+    ["C"] = { api.tree.change_root_to_node, opts "CD" },
+  }
+
+  for k, v in pairs(useful_keys) do
+    vim.keymap.set('n', k, v[1], v[2])
+  end
+end
+
+
+require('nvim-tree').setup {
+  auto_reload_on_write = false,
+  disable_netrw = false,
+  hijack_cursor = false,
+  hijack_netrw = true,
+  hijack_unnamed_buffer_when_opening = false,
+  sort_by = "name",
+  root_dirs = {},
+  prefer_startup_root = false,
+  sync_root_with_cwd = true,
+  reload_on_bufenter = false,
+  respect_buf_cwd = true,
+  on_attach = on_attach,
+  remove_keymaps = false,
+  select_prompts = false,
+  view = {
+    adaptive_size = false,
+    centralize_selection = true,
+    width = 30,
+    hide_root_folder = false,
+    side = "left",
+    preserve_window_proportions = false,
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes",
+    float = {
+      enable = false,
+      quit_on_focus_loss = true,
+      open_win_config = {
+        relative = "editor",
+        border = "rounded",
+        width = 30,
+        height = 30,
+        row = 1,
+        col = 1,
+      },
+    },
+  },
+  renderer = {
+    add_trailing = false,
+    group_empty = false,
+    highlight_git = true,
+    full_name = false,
+    highlight_opened_files = "none",
+    root_folder_label = ":t",
+    indent_width = 2,
+    indent_markers = {
+      enable = false,
+      inline_arrows = true,
+      icons = {
+        corner = "└",
+        edge = "│",
+        item = "│",
+        none = " ",
+      },
+    },
+    icons = {
+      webdev_colors = use_icons,
+      git_placement = "before",
+      padding = " ",
+      symlink_arrow = " ➛ ",
+      show = {
+        file = use_icons,
+        folder = use_icons,
+        folder_arrow = use_icons,
+        git = use_icons,
+      },
+      glyphs = {
+        default = icons.ui.Text,
+        symlink = icons.ui.FileSymlink,
+        bookmark = icons.ui.BookMark,
+        folder = {
+          arrow_closed = icons.ui.TriangleShortArrowRight,
+          arrow_open = icons.ui.TriangleShortArrowDown,
+          default = icons.ui.Folder,
+          open = icons.ui.FolderOpen,
+          empty = icons.ui.EmptyFolder,
+          empty_open = icons.ui.EmptyFolderOpen,
+          symlink = icons.ui.FolderSymlink,
+          symlink_open = icons.ui.FolderOpen,
+        },
+        git = {
+          unstaged = icons.git.FileUnstaged,
+          staged = icons.git.FileStaged,
+          unmerged = icons.git.FileUnmerged,
+          renamed = icons.git.FileRenamed,
+          untracked = icons.git.FileUntracked,
+          deleted = icons.git.FileDeleted,
+          ignored = icons.git.FileIgnored,
+        },
+      },
+    },
+    special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+    symlink_destination = true,
+  },
+  hijack_directories = {
+    enable = false,
+    auto_open = true,
+  },
+  update_focused_file = {
+    enable = true,
+    debounce_delay = 15,
+    update_root = true,
+    ignore_list = {},
+  },
+  diagnostics = {
+    enable = use_icons,
+    show_on_dirs = false,
+    show_on_open_dirs = true,
+    debounce_delay = 50,
+    severity = {
+      min = vim.diagnostic.severity.HINT,
+      max = vim.diagnostic.severity.ERROR,
+    },
+    icons = {
+      hint = icons.diagnostics.BoldHint,
+      info = icons.diagnostics.BoldInformation,
+      warning = icons.diagnostics.BoldWarning,
+      error = icons.diagnostics.BoldError,
+    },
+  },
+  filters = {
+    dotfiles = false,
+    git_clean = false,
+    no_buffer = false,
+    custom = { "node_modules", "\\.cache" },
+    exclude = {},
+  },
+  filesystem_watchers = {
+    enable = true,
+    debounce_delay = 50,
+    ignore_dirs = {},
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    show_on_dirs = true,
+    show_on_open_dirs = true,
+    timeout = 200,
+  },
+  actions = {
+    use_system_clipboard = true,
+    change_dir = {
+      enable = false,
+      global = true,
+      restrict_above_cwd = true,
+    },
+    expand_all = {
+      max_folder_discovery = 300,
+      exclude = {},
+    },
+    file_popup = {
+      open_win_config = {
+        col = 1,
+        row = 1,
+        relative = "cursor",
+        border = "shadow",
+        style = "minimal",
+      },
+    },
+    open_file = {
+      quit_on_open = false,
+      resize_window = false,
+      window_picker = {
+        enable = true,
+        picker = "default",
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {
+          filetype = { "notify", "lazy", "qf", "diff", "fugitive", "fugitiveblame" },
+          buftype = { "nofile", "terminal", "help" },
+        },
+      },
+    },
+    remove_file = {
+      close_window = true,
+    },
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true,
+  },
+  live_filter = {
+    prefix = "[FILTER]: ",
+    always_show_folders = true,
+  },
+  tab = {
+    sync = {
+      open = false,
+      close = false,
+      ignore = {},
+    },
+  },
+  notify = {
+    threshold = vim.log.levels.INFO,
+  },
+  log = {
+    enable = false,
+    truncate = false,
+    types = {
+      all = false,
+      config = false,
+      copy_paste = false,
+      dev = false,
+      diagnostics = false,
+      git = false,
+      profile = false,
+      watcher = false,
+    },
+  },
+  system_open = {
+    cmd = nil,
+    args = {},
+  },
+}
+
+
+---------------- treesitter config ----------------
+
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { 'rust', 'python', 'c', 'cpp', 'toml', 'vim', 'vimdoc', 'vue' },
+  sync_install = true,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+require('nvim-web-devicons').setup {
+  color_icons = true,
+  default = true,
+  strict = true,
+}
+
+
+
+
+local function buf_kill(kill_command, bufnr, force)
+  kill_command = 'bd'
+
+  local bo = vim.bo
+  local api = vim.api
+  local fmt = string.format
+  local fnamemodify = vim.fn.fnamemodify
+
+  if bufnr == 0 or bufnr == nil then
+    bufnr = api.nvim_get_current_buf()
+  end
+
+  local bufname = api.nvim_buf_get_name(bufnr)
+
+  if not force then
+    local warning
+    if bo[bufnr].modified then
+      warning = fmt([[No write since last change for (%s)]], fnamemodify(bufname, ":t"))
+    elseif api.nvim_buf_get_option(bufnr, "buftype") == "terminal" then
+      warning = fmt([[Terminal %s will be killed]], bufname)
+    end
+    if warning then
+      vim.ui.input({
+        prompt = string.format([[%s. Close it anyway? [y]es or [n]o (default: no): ]], warning),
+      }, function(choice)
+        if choice ~= nil and choice:match "ye?s?" then buf_kill(kill_command, bufnr, true) end
+      end)
+      return
+    end
+  end
+
+  -- Get list of windows IDs with the buffer to close
+  local windows = vim.tbl_filter(function(win)
+    return api.nvim_win_get_buf(win) == bufnr
+  end, api.nvim_list_wins())
+
+  if force then
+    kill_command = kill_command .. "!"
+  end
+
+  -- Get list of active buffers
+  local buffers = vim.tbl_filter(function(buf)
+    return api.nvim_buf_is_valid(buf) and bo[buf].buflisted
+  end, api.nvim_list_bufs())
+
+  -- If there is only one buffer (which has to be the current one), vim will
+  -- create a new buffer on :bd.
+  -- For more than one buffer, pick the previous buffer (wrapping around if necessary)
+  if #buffers > 1 and #windows > 0 then
+    for i, v in ipairs(buffers) do
+      if v == bufnr then
+        local prev_buf_idx = i == 1 and #buffers or (i - 1)
+        local prev_buffer = buffers[prev_buf_idx]
+        for _, win in ipairs(windows) do
+          api.nvim_win_set_buf(win, prev_buffer)
+        end
+      end
+    end
+  end
+
+  -- Check if buffer still exists, to ensure the target buffer wasn't killed
+  -- due to options like bufhidden=wipe.
+  if api.nvim_buf_is_valid(bufnr) and bo[bufnr].buflisted then
+    vim.cmd(string.format("%s %d", kill_command, bufnr))
+  end
+end
+
+local opts = vim.tbl_deep_extend("force", { force = true }, {})
+vim.api.nvim_create_user_command('BufferKill', buf_kill, opts)
---
+++ init.vim
@@ -0,0 +1,269 @@
+if has("autocmd")
+  au BufReadPost * if line("'\"") > 1 && line("'\"") <= 
+        \ line("$") | exe "normal! g'\"" | endif
+endif
+
+call plug#begin('~/.vim/plugged')
+
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'nvim-treesitter/nvim-treesitter', { 'do': ':TSUpdate' }
+Plug 'shatur/neovim-ayu'
+Plug 'nvim-lualine/lualine.nvim'
+Plug 'nvim-tree/nvim-web-devicons'
+Plug 'akinsho/bufferline.nvim'
+Plug 'akinsho/toggleterm.nvim'
+Plug 'lewis6991/gitsigns.nvim'
+Plug 'nvim-tree/nvim-tree.lua'
+"Plug 'puremourning/vimspector'
+call plug#end()
+
+inoremap <silent><expr><TAB> coc#pum#visible() ? coc#pum#next(1) : CheckBackspace() ? "\<Tab>" : coc#refresh()
+inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+
+" Make <CR> to accept selected completion item or notify coc.nvim to format
+" <C-g>u breaks current undo, please make your own choice
+inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm(): "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+
+
+function! CheckBackspace() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+endfunction
+
+" Use <c-space> to trigger completion
+inoremap <silent><expr> <c-space> coc#refresh()
+
+" Make <CR> auto-select the first completion item and notify coc.nvim to
+" format on enter, <cr> could be remapped by other vim plugin
+
+nmap <silent> [d <Plug>(coc-diagnostic-prev)
+nmap <silent> ]d <Plug>(coc-diagnostic-next)
+
+" GoTo code navigation.
+nmap <silent> gd <Plug>(coc-definition)
+nmap <silent> gy <Plug>(coc-type-definition)
+nmap <silent> gi <Plug>(coc-implementation)
+nmap <silent> gr <Plug>(coc-references)
+
+" Use K to show documentation in preview window.
+nnoremap <silent> K :call <SID>show_documentation()<CR>
+
+
+let g:markdown_fenced_languages = ['vim','help']
+
+function! s:show_documentation()
+  if (index(['vim','help'], &filetype) >= 0)
+    call CocActionAsync('doHover')
+    " execute 'h '.expand('<cword>')
+  elseif (coc#rpc#ready())
+    call CocActionAsync('doHover')
+  else
+    execute '!' . &keywordprg . " " . expand('<cword>')
+  endif
+endfunction
+
+
+" Highlight the symbol and its references when holding the cursor.
+autocmd CursorHold * silent call CocActionAsync('highlight')
+"autocmd BufWritePre * silent call CocActionAsync('format')
+
+
+" Symbol renaming.
+nmap <leader>r <Plug>(coc-rename)
+
+xmap <leader>f <Plug>(coc-format-selected)<cr>
+nmap <leader>f <Plug>(coc-format-selected)<cr>
+map <silent> <S-M-f> :CocCommand editor.action.formatDocument<cr>
+
+augroup mygroup
+  autocmd!
+  " Setup formatexpr specified filetype(s).
+  autocmd FileType typescript,json setl formatexpr=CocAction('formatSelected')
+  " Update signature help on jump placeholder.
+  autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
+augroup end
+
+" Applying codeAction to the selected region.
+" Example: `<leader>aap` for current paragraph
+xmap <leader>a  <Plug>(coc-codeaction-selected)
+nmap <leader>a  <Plug>(coc-codeaction-selected)
+
+nmap <leader>ac  <Plug>(coc-codeaction-cursor)
+nmap <leader>as  <Plug>(coc-codeaction-source)
+nmap <leader>qf  <Plug>(coc-fix-current)
+
+" Run the Code Lens action on the current line.
+nmap <leader>cl  <Plug>(coc-codelens-action)
+
+" Map function and class text objects
+" NOTE: Requires 'textDocument.documentSymbol' support from the language server.
+xmap if <Plug>(coc-funcobj-i)
+omap if <Plug>(coc-funcobj-i)
+xmap af <Plug>(coc-funcobj-a)
+omap af <Plug>(coc-funcobj-a)
+xmap ic <Plug>(coc-classobj-i)
+omap ic <Plug>(coc-classobj-i)
+xmap ac <Plug>(coc-classobj-a)
+omap ac <Plug>(coc-classobj-a)
+
+" Remap <C-f> and <C-b> for scroll float windows/popups.
+if has('nvim-0.4.0') || has('patch-8.2.0750')
+  nnoremap <silent><nowait><expr> <C-d> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-d>"
+  nnoremap <silent><nowait><expr> <C-u> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-u>"
+  inoremap <silent><nowait><expr> <C-d> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
+  inoremap <silent><nowait><expr> <C-u> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
+  vnoremap <silent><nowait><expr> <C-d> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-d>"
+  vnoremap <silent><nowait><expr> <C-u> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-u>"
+endif
+
+
+" Use CTRL-S for selections ranges.
+" Requires 'textDocument/selectionRange' support of language server.
+nmap <silent> <C-s> <Plug>(coc-range-select)
+xmap <silent> <C-s> <Plug>(coc-range-select)
+
+command! -nargs=0 Format :call   CocActionAsync('format')
+command! -nargs=? Fold :call     CocAction('fold', <f-args>)
+command! -nargs=0 OR   :call     CocActionAsync('runCommand', 'editor.action.organizeImport')
+
+" Mappings for CoCList
+nnoremap <silent><nowait> <space>d :<C-u>CocList diagnostics<cr>
+nnoremap <silent><nowait> <space>x :<C-u>CocList extensions<cr>
+nnoremap <silent><nowait> <space>c :<C-u>CocList commands<cr>
+nnoremap <silent><nowait> <space>o :<C-u>CocList outline<cr>
+nnoremap <silent><nowait> <space>s :<C-u>CocList symbols<cr>
+nnoremap <silent><nowait> <space>m :<C-u>CocList marketplace<cr>
+nnoremap <silent><nowait> <space>b :<C-u>CocList buffers<cr>
+nnoremap <silent><nowait> <space>j :<C-u>CocNext<CR>
+nnoremap <silent><nowait> <space>k :<C-u>CocPrev<CR>
+nnoremap <silent><nowait> <space>p :<C-u>CocListResume<CR>
+nnoremap <silent><nowait> <space>e :<C-u>NvimTreeToggle<CR>
+nnoremap <silent><nowait> <space>f :<C-u>CocList files<CR>
+nnoremap <silent><nowait> <space>l :<C-u>CocList<CR>
+nnoremap <silent><nowait> <space>g :<C-u>CocList grep<CR>
+nnoremap <silent><nowait> <space>w :<C-u>CocList words<CR>
+nnoremap <silent><nowait> <space>r :<C-u>CocList mru<CR>
+nnoremap <silent><nowait> <space>v :<C-u>CocList vimcommands<CR>
+
+
+"Goto keymap
+map <silent> gj :<C-u>CocCommand document.jumpToNextSymbol<cr>
+map <silent> gk :<C-u>CocCommand document.jumpToPrevSymbol<cr>
+
+map <silent> gn :<C-u>bn<cr>
+map <silent> gp :<C-u>bp<cr>
+map <silent> gq :<C-u>b#\|bd#<cr>
+"map <silent> gq <cmd>BufferKill<cr>
+map <silent> gW :<C-w>q<cr>
+map <silent> gQ :<C-u>%bd!\|e#\|bd#<cr>
+map <silent> ga :<C-u>e#<cr>
+
+map <silent> gh 0
+map <silent> gs ^
+map <silent> gl $
+map <silent> gc M
+map <silent> ge G
+
+
+nnoremap <silent><A-1> <Cmd>exe v:count . "ToggleTerm direction=horizontal"<CR>
+nnoremap <silent><A-2> <Cmd>exe v:count . "ToggleTerm direction=vertical"<CR>
+nnoremap <silent><A-3> <Cmd>exe v:count . "ToggleTerm direction=float"<CR>
+
+autocmd TermEnter term://*toggleterm#* tnoremap <silent><A-1> <Cmd>exe v:count . "ToggleTerm"<CR>
+autocmd TermEnter term://*toggleterm#* tnoremap <silent><A-2> <Cmd>exe v:count . "ToggleTerm"<CR>
+autocmd TermEnter term://*toggleterm#* tnoremap <silent><A-3> <Cmd>exe v:count . "ToggleTerm"<CR>
+
+"nnoremap <silent><A-1> <Cmd>exe v:count . "ToggleTerm"<CR>
+"inoremap <silent><A-1> <Esc><Cmd>exe v:count . "ToggleTerm"<CR>
+
+map <m-h> <c-w>h
+map <m-l> <c-w>l
+map <m-j> <c-w>j
+map <m-k> <c-w>kl
+
+
+
+tnoremap <m-h> <c-\><c-n><c-w>h
+tnoremap <m-l> <c-\><c-n><c-w>l
+tnoremap <m-j> <c-\><c-n><c-w>j
+tnoremap <m-k> <c-\><c-n><c-w>k
+tnoremap <m--> <c-\><c-n>"0pa
+tnoremap <m-q> <c-\><c-n>
+
+
+""""""""""""""" resize window """"""""""""""""""
+nmap <silent><c-m-h> :vertical resize -2<cr>
+nmap <silent><c-m-l> :vertical resize +2<cr>
+nmap <silent><c-m-j> :resize -2<cr>
+nmap <silent><c-m-k> :resize +2<cr>
+
+inoremap <silent><c-m-h> <c-\><c-n>:vertical resize -2<cr>a
+inoremap <silent><c-m-l> <c-\><c-n>:vertical resize +2<cr>a
+inoremap <silent><c-m-j> <c-\><c-n>:resize -2<cr>a
+inoremap <silent><c-m-k> <c-\><c-n>:resize +2<cr>a
+
+tnoremap <silent><c-m-h> <c-\><c-n>:vertical resize -2<cr>a
+tnoremap <silent><c-m-l> <c-\><c-n>:vertical resize +2<cr>a
+tnoremap <silent><c-m-j> <c-\><c-n>:resize -2<cr>a
+tnoremap <silent><c-m-k> <c-\><c-n>:resize +2<cr>a
+
+
+nnoremap <F3> :set hlsearch! <CR>
+
+
+"Edit config setting file by $MYVIMRC
+nnoremap <silent><nowait> <S-A-c> :e $MYVIMRC<CR>
+nnoremap <silent><nowait> <S-A-l> :e ~/AppData/Local/nvim/conf.lua<CR>
+nnoremap <silent><nowait> <S-A-r> :<c-u>so$MYVIMRC\|colo ayu<cr>
+
+
+
+" Theme setup
+
+set termguicolors
+
+
+let g:airline_powerline_fonts = 1
+
+set number
+set notimeout
+set ignorecase
+set smartcase
+
+
+set nobackup
+set nowritebackup
+
+set updatetime=100
+
+" Don't pass messages to |ins-completion-menu|.
+set shortmess+=c
+
+set foldmethod=manual
+set foldexpr=''
+
+"set smartindent
+filetype indent plugin on
+set expandtab
+set tabstop=2
+set softtabstop=2
+set shiftwidth=2
+set signcolumn=yes
+
+set nohlsearch
+set cursorline
+set fillchars=fold:\ ,vert:\│,eob:\ ,msgsep:‾
+
+set wildmenu
+set wildmode=list:longest,full
+set wildignore=*.dll,*.exe,*.jpg,*.gif,*.png
+
+set shell=pwsh.exe
+"let &shellcmdflag = '-c' "for bash shell
+let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
+let &shellpipe    = '| Out-File -Encoding UTF8 %s'
+let &shellredir   = '| Out-File -Encoding UTF8 %s'
+set shellxquote= shellxquote=
+
+
+runtime conf.lua
