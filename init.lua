e9cb033174b7798bca1cc11de217d4b47a3fd1ae
Robert <zhruith@gmail.com>
2024-12-26 15:21:07 +0800

    update lazy.nvim

--- init.lua
+++ init.lua
@@ -2,38 +2,110 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
-vim.opt.rtp:prepend(lazypath)
+
 -- Setup lazy.nvim
+vim.opt.rtp:prepend(lazypath)
 vim.g.lazy_did_setup = false
 require("lazy").setup({
   spec = {
     { 'neoclide/coc.nvim', branch = "release" },
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-tree/nvim-tree.lua',
-    'nvim-lualine/lualine.nvim',
-    'nvim-tree/nvim-web-devicons',
-    'akinsho/bufferline.nvim',
-    'akinsho/toggleterm.nvim',
-    'shatur/neovim-ayu',
-    'lewis6991/gitsigns.nvim',
-    'easymotion/vim-easymotion',
+    {
+      'shatur/neovim-ayu',
+      -- event = "VimEnter",
+      config = function()
+        require("ayu").setup {
+          mirage = false, terminal = false,
+          overrides = function()
+            return { NormalFloat = { bg = "black" }, CocHighlightText = { bg = "#273747" }, }
+          end,
+        }
+        require("ayu").colorscheme()
+      end
+    },
+    {
+      'nvim-lualine/lualine.nvim',
+      event = "BufReadPost",
+      opts = {
+        options = {
+          globalstatus = true,
+          component_separators = { left = "", right = "" },
+          disabled_filetypes = { statusline = { 'list', '' }, winbar = {} },
+          section_separators = {},
+        },
+        sections = {
+          lualine_c = { 'filename', 'g:coc_status' },
+          lualine_y = {
+            { 'datetime', style = '%H:%M' },
+            { 'progress', fmt = function() return "%L" end, },
+            'progress',
+          },
+        }
+      }
+    },
+    {
+      'akinsho/toggleterm.nvim',
+      cmd = "ToggleTerm",
+      opts = {
+        persist_mode = false,
+        persist_size = true,
+        winbar = {
+          enabled = false,
+        },
+        size = function(term)
+          if term.direction == 'horizontal' then
+            return 15
+          elseif term.direction == 'vertical' then
+            return vim.o.columns * 0.4
+          else
+            return 20
+          end
+        end,
+      }
+    },
+    {
+      'nvim-treesitter/nvim-treesitter',
+      event = "BufReadPost",
+      config = function()
+        require('nvim-treesitter.configs').setup {
+          ensure_installed = {},
+          sync_install = false, auto_install = true,
+          highlight = { enable = true, additional_vim_regex_highlighting = true, },
+        }
+      end,
+    },
+    {
+      'akinsho/bufferline.nvim',
+      event = "BufReadPost",
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      opts = {
+        options = {
+          always_show_bufferline = false,
+          offsets = { { filetype = 'NvimTree' }, { filetype = 'netrw' } },
+        },
+      },
+    },
+    {
+      'nvim-tree/nvim-tree.lua',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      keys = { { "<space>e", "<cmd>NvimTreeToggle<cr>", mode = { "n", "x" } } },
+      opts = {},
+    },
+    {
+      'easymotion/vim-easymotion',
+      keys = { { "s", "<Plug>(easymotion-bd-w)", mode = "n" } },
+      opts = { EasyMotion_do_mapping = 0, EasyMotion_smartcase = 1 }
+    },
+    {
+      'lewis6991/gitsigns.nvim',
+      opts = {},
+    },
     -- 'puremourning/vimspector',
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "ayu" } },
-  -- automatically check for plugin updates
   checker = { enabled = false },
 })
 -- global settings
@@ -68,10 +140,6 @@ vim.cmd('set fillchars+=eob:\\ ')
 vim.cmd('set shell=nu')
 vim.o.shellcmdflag = '-c '
 vim.cmd('set shellxquote= shellxquote=')
-vim.g.EasyMotion_do_mapping = 0
-vim.g.EasyMotion_smartcase = 1
-
-require "conf" -- loading other plugs setting
 
 -- coc.nvim default settings
 local keyset = vim.keymap.set
@@ -151,31 +219,31 @@ vim.api.nvim_create_autocmd("User", {
 })
 -- Apply codeAction to the selected region
 -- Example: `<leader>aap` for current paragraph
-local opts = { silent = true, nowait = true }
-keyset("x", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
-keyset("n", "<leader>a", "<Plug>(coc-codeaction-selected)", opts)
+local opts2 = { silent = true, nowait = true }
+keyset("x", "<leader>a", "<Plug>(coc-codeaction-selected)", opts2)
+keyset("n", "<leader>a", "<Plug>(coc-codeaction-selected)", opts2)
 -- Remap keys for apply code actions at the cursor position.
-keyset("n", "<leader>ac", "<Plug>(coc-codeaction-cursor)", opts)
+keyset("n", "<leader>ac", "<Plug>(coc-codeaction-cursor)", opts2)
 -- Remap keys for apply source code actions for current file.
-keyset("n", "<leader>as", "<Plug>(coc-codeaction-source)", opts)
+keyset("n", "<leader>as", "<Plug>(coc-codeaction-source)", opts2)
 -- Apply the most preferred quickfix action on the current line.
-keyset("n", "<leader>qf", "<Plug>(coc-fix-current)", opts)
+keyset("n", "<leader>qf", "<Plug>(coc-fix-current)", opts2)
 -- Remap keys for apply refactor code actions.
 keyset("n", "<leader>re", "<Plug>(coc-codeaction-refactor)", silent)
 keyset("x", "<leader>r", "<Plug>(coc-codeaction-refactor-selected)", silent)
 keyset("n", "<leader>r", "<Plug>(coc-codeaction-refactor-selected)", silent)
 -- Run the Code Lens actions on the current line
-keyset("n", "<leader>cl", "<Plug>(coc-codelens-action)", opts)
+keyset("n", "<leader>cl", "<Plug>(coc-codelens-action)", opts2)
 -- Map function and class text objects
 -- NOTE: Requires 'textDocument.documentSymbol' support from the language server
-keyset("x", "if", "<Plug>(coc-funcobj-i)", opts)
-keyset("o", "if", "<Plug>(coc-funcobj-i)", opts)
-keyset("x", "af", "<Plug>(coc-funcobj-a)", opts)
-keyset("o", "af", "<Plug>(coc-funcobj-a)", opts)
-keyset("x", "ic", "<Plug>(coc-classobj-i)", opts)
-keyset("o", "ic", "<Plug>(coc-classobj-i)", opts)
-keyset("x", "ac", "<Plug>(coc-classobj-a)", opts)
-keyset("o", "ac", "<Plug>(coc-classobj-a)", opts)
+keyset("x", "if", "<Plug>(coc-funcobj-i)", opts2)
+keyset("o", "if", "<Plug>(coc-funcobj-i)", opts2)
+keyset("x", "af", "<Plug>(coc-funcobj-a)", opts2)
+keyset("o", "af", "<Plug>(coc-funcobj-a)", opts2)
+keyset("x", "ic", "<Plug>(coc-classobj-i)", opts2)
+keyset("o", "ic", "<Plug>(coc-classobj-i)", opts2)
+keyset("x", "ac", "<Plug>(coc-classobj-a)", opts2)
+keyset("o", "ac", "<Plug>(coc-classobj-a)", opts2)
 -- Remap <C-f> and <C-b> to scroll float windows/popups
 ---@diagnostic disable-next-line: redefined-local
 local opts = { silent = true, nowait = true, expr = true }
@@ -197,78 +265,77 @@ vim.opt.statusline:prepend("%{coc#status()}%{get(b:,'coc_current_function','')}"
 -- Mappings for CoCList
 -- code actions and coc stuff
 ---@diagnostic disable-next-line: redefined-local
-local opts = { silent = true, nowait = true, expr = false }
-keyset("n", "<space>d", ":CocList diagnostics<cr>", opts)
-keyset("n", "<space>x", ":CocList extensions<cr>", opts)
-keyset("n", "<space>c", ":CocList commands<cr>", opts)
-keyset("n", "<space>o", ":CocCommand session.load<cr>", opts)
-keyset("n", "<space>s", ":CocList outline<cr>", opts)
-keyset("n", "<space>S", ":CocList -I symbols<cr>", opts)
-keyset("n", "<space>j", ":CocNext<cr>", opts)
-keyset("n", "<space>k", ":CocPrev<cr>", opts)
-keyset("n", "<space>p", ":CocListResume<cr>", opts)
-keyset("n", "<space>m", ":CocList marketplace<cr>", opts)
-keyset("n", "<space>b", ":CocList buffers<cr>", opts)
-keyset("n", "<space>e", ":NvimTreeToggle<cr>", opts)
-keyset("n", "<space>f", ":CocList files<cr>", opts)
-keyset("n", "<space>l", ":CocList<cr>", opts)
-keyset("n", "<space>g", ":CocList grep<cr>", opts)
-keyset("n", "<space>w", ":CocList words<cr>", opts)
-keyset("n", "<space>r", ":CocList mru<cr>", opts)
-keyset("n", "<space>v", ":CocList vimcommands<cr>", opts)
-keyset("n", "<space>U", ":CocUpdate<cr>", opts)
-keyset("n", "<space>L", ":Lazy<cr>", opts)
--- g keyshot
+local opts3 = { silent = true, nowait = true, expr = false }
+keyset("n", "<space>d", ":CocList diagnostics<cr>", opts3)
+keyset("n", "<space>x", ":CocList extensions<cr>", opts3)
+keyset("n", "<space>c", ":CocList commands<cr>", opts3)
+keyset("n", "<space>o", ":CocCommand session.load<cr>", opts3)
+keyset("n", "<space>s", ":CocList outline<cr>", opts3)
+keyset("n", "<space>S", ":CocList -I symbols<cr>", opts3)
+keyset("n", "<space>j", ":CocNext<cr>", opts3)
+keyset("n", "<space>k", ":CocPrev<cr>", opts3)
+keyset("n", "<space>p", ":CocListResume<cr>", opts3)
+keyset("n", "<space>m", ":CocList marketplace<cr>", opts3)
+keyset("n", "<space>b", ":CocList buffers<cr>", opts3)
+keyset("n", "<space>f", ":CocList files<cr>", opts3)
+keyset("n", "<space>l", ":CocList<cr>", opts3)
+keyset("n", "<space>g", ":CocList grep<cr>", opts3)
+keyset("n", "<space>w", ":CocList words<cr>", opts3)
+keyset("n", "<space>r", ":CocList mru<cr>", opts3)
+keyset("n", "<space>v", ":CocList vimcommands<cr>", opts3)
+keyset("n", "<space>U", ":CocUpdate<cr>", opts3)
+keyset("n", "<space>L", ":Lazy<cr>", opts3)
 
-keyset("n", "gn", ":bn<cr>", opts)
-keyset("n", "gp", ":bp<cr>", opts)
-keyset("n", "gq", ":bd%<cr>", opts)
-keyset("n", "gQ", ":%bd!|e#|bd#<cr>", opts)
-keyset("n", "ga", ":e#<cr>", opts)
-keyset("n", "gw", "<Plug>(coc-float-jump)", opts)
-keyset({ "n", "x" }, "gh", "0", opts)
-keyset({ "n", "x" }, "gs", "^", opts)
-keyset({ "n", "x" }, "gl", "$", opts)
-keyset("n", "gc", "M", opts)
-keyset("n", "ge", "G", opts)
+-- g keyshot
+keyset("n", "gn", ":bn<cr>", opts3)
+keyset("n", "gp", ":bp<cr>", opts3)
+keyset("n", "gq", ":bd%<cr>", opts3)
+keyset("n", "gQ", ":%bd!|e#|bd#<cr>", opts3)
+keyset("n", "ga", ":e#<cr>", opts3)
+keyset("n", "gw", "<Plug>(coc-float-jump)", opts3)
+keyset({ "n", "x" }, "gh", "0", opts3)
+keyset({ "n", "x" }, "gs", "^", opts3)
+keyset({ "n", "x" }, "gl", "$", opts3)
+keyset("n", "gc", "M", opts3)
+keyset("n", "ge", "G", opts3)
 -- misc keyshot
-keyset("n", "s", "<Plug>(easymotion-bd-w)", opts)
-keyset("n", "<S-A-f>", ":CocCommand editor.action.formatDocument<cr>", opts)
-keyset("n", "<S-A-c>", ":e $MYVIMRC<cr>", opts)
-keyset("n", "<S-A-r>", ":luafile %<cr>", opts)
+keyset("n", "<Tab>", ":BufferLinePick<cr>", opts3)
+keyset("n", "<S-A-f>", ":CocCommand editor.action.formatDocument<cr>", opts3)
+keyset("n", "<S-A-c>", ":e $MYVIMRC<cr>", opts3)
+keyset("n", "<S-A-r>", ":luafile %<cr>", opts3)
 keyset({ "n", "x", "i" }, "<C-s>", "<Esc>:w<cr>", silent)
-keyset("n", "<M-h>", function() vim.fn.CocActionAsync('highlight') end, opts)
-keyset("n", "(", ":CocCommand document.jumpToNextSymbol<cr>", opts)
-keyset("n", ")", ":CocCommand document.jumpToPrevSymbol<cr>", opts)
+keyset("n", "<M-h>", function() vim.fn.CocActionAsync('highlight') end, opts3)
+keyset("n", "(", ":CocCommand document.jumpToNextSymbol<cr>", opts3)
+keyset("n", ")", ":CocCommand document.jumpToPrevSymbol<cr>", opts3)
 -- moving cursor
-keyset("n", "<C-h>", "<C-w>h", opts)
-keyset("n", "<C-l>", "<C-w>l", opts)
-keyset("n", "<C-j>", "<C-w>j", opts)
-keyset("n", "<C-k>", "<C-w>k", opts)
-keyset({ "i", "t" }, "<C-h>", "<C-\\><C-n><C-w>h", opts)
-keyset({ "i", "t" }, "<C-l>", "<C-\\><C-n><C-w>l", opts)
-keyset({ "i", "t" }, "<C-j>", "<C-\\><C-n><C-w>j", opts)
-keyset({ "i", "t" }, "<C-k>", "<C-\\><C-n><C-w>k", opts)
-keyset("t", "<C-v>", '<C-\\><C-n>"0pa', opts)
-keyset("t", "<C-q>", "<C-\\><C-n>", opts)
+keyset("n", "<C-h>", "<C-w>h", opts3)
+keyset("n", "<C-l>", "<C-w>l", opts3)
+keyset("n", "<C-j>", "<C-w>j", opts3)
+keyset("n", "<C-k>", "<C-w>k", opts3)
+keyset({ "i", "t" }, "<C-h>", "<C-\\><C-n><C-w>h", opts3)
+keyset({ "i", "t" }, "<C-l>", "<C-\\><C-n><C-w>l", opts3)
+keyset({ "i", "t" }, "<C-j>", "<C-\\><C-n><C-w>j", opts3)
+keyset({ "i", "t" }, "<C-k>", "<C-\\><C-n><C-w>k", opts3)
+keyset("t", "<C-v>", '<C-\\><C-n>"0pa', opts3)
+keyset("t", "<C-q>", "<C-\\><C-n>", opts3)
 -- moving lines
-keyset("n", "<M-j>", ":m .+1<cr>", opts)
-keyset("n", "<M-k>", ":m .-2<cr>", opts)
-keyset("i", "<M-j>", "<Esc>:m .+1<cr>a", opts)
-keyset("i", "<M-k>", "<Esc>:m .-2<cr>a", opts)
-keyset("v", "<M-j>", ":m '>+1<cr>gv-gv", opts)
-keyset("v", "<M-k>", ":m '<-2<cr>gv-gv", opts)
-keyset("v", "<", "<gv", opts)
-keyset("v", ">", ">gv", opts)
+keyset("n", "<M-j>", ":m .+1<cr>", opts3)
+keyset("n", "<M-k>", ":m .-2<cr>", opts3)
+keyset("i", "<M-j>", "<Esc>:m .+1<cr>a", opts3)
+keyset("i", "<M-k>", "<Esc>:m .-2<cr>a", opts3)
+keyset("v", "<M-j>", ":m '>+1<cr>gv-gv", opts3)
+keyset("v", "<M-k>", ":m '<-2<cr>gv-gv", opts3)
+keyset("v", "<", "<gv", opts3)
+keyset("v", ">", ">gv", opts3)
 -- resize window
-keyset("n", "<S-M-h>", ":vertical resize -2<cr>", opts)
-keyset("n", "<S-M-l>", ":vertical resize +2<cr>", opts)
-keyset("n", "<S-M-j>", ":resize -2<cr>", opts)
-keyset("n", "<S-M-k>", ":resize +2<cr>", opts)
-keyset({ "i", "t" }, "<S-M-h>", "<C-\\><C-n>:vertical resize -2<cr>a", opts)
-keyset({ "i", "t" }, "<S-M-l>", "<C-\\><C-n>:vertical resize +2<cr>a", opts)
-keyset({ "i", "t" }, "<S-M-j>", "<C-\\><C-n>:resize -2<cr>a", opts)
-keyset({ "i", "t" }, "<S-M-k>", "<C-\\><C-n>:resize +2<cr>a", opts)
+keyset("n", "<S-M-h>", ":vertical resize -2<cr>", opts3)
+keyset("n", "<S-M-l>", ":vertical resize +2<cr>", opts3)
+keyset("n", "<S-M-j>", ":resize -2<cr>", opts3)
+keyset("n", "<S-M-k>", ":resize +2<cr>", opts3)
+keyset({ "i", "t" }, "<S-M-h>", "<C-\\><C-n>:vertical resize -2<cr>a", opts3)
+keyset({ "i", "t" }, "<S-M-l>", "<C-\\><C-n>:vertical resize +2<cr>a", opts3)
+keyset({ "i", "t" }, "<S-M-j>", "<C-\\><C-n>:resize -2<cr>a", opts3)
+keyset({ "i", "t" }, "<S-M-k>", "<C-\\><C-n>:resize +2<cr>a", opts3)
 
 local function terminal_create(cmd)
   vim.cmd(string.format('%s | terminal', cmd))
@@ -296,8 +363,8 @@ vim.api.nvim_create_autocmd({ "BufHidden" }, {
   end
 })
 
-keyset({ "n", "i", "t" }, "<M-+>", function() terminal_create('vsplit | wincmd l') end, opts)
-keyset({ "n", "i", "t" }, "<M-_>", function() terminal_create('split | wincmd j') end, opts)
+keyset({ "n", "i", "t" }, "<M-+>", function() terminal_create('vsplit | wincmd l') end, opts3)
+keyset({ "n", "i", "t" }, "<M-_>", function() terminal_create('split | wincmd j') end, opts3)
 
 -- set terminal
 vim.cmd([[nnoremap <silent><M-1> <Cmd>exe v:count . "ToggleTerm direction=horizontal"<CR>]])
@@ -306,9 +373,9 @@ vim.cmd([[nnoremap <silent><M-3> <Cmd>exe v:count . "ToggleTerm direction=float"
 vim.api.nvim_create_autocmd("TermEnter", {
   pattern = "term://*toggleterm#*",
   callback = function()
-    keyset("t", "<M-1>", string.format('<Cmd>exe %d . "ToggleTerm"<CR>', vim.v.count), opts)
-    keyset("t", "<M-2>", string.format('<Cmd>exe %d . "ToggleTerm"<CR>', vim.v.count), opts)
-    keyset("t", "<M-3>", string.format('<Cmd>exe %d . "ToggleTerm"<CR>', vim.v.count), opts)
+    keyset("t", "<M-1>", string.format('<Cmd>exe %d . "ToggleTerm"<CR>', vim.v.count), opts3)
+    keyset("t", "<M-2>", string.format('<Cmd>exe %d . "ToggleTerm"<CR>', vim.v.count), opts3)
+    keyset("t", "<M-3>", string.format('<Cmd>exe %d . "ToggleTerm"<CR>', vim.v.count), opts3)
   end,
 })
 vim.api.nvim_create_autocmd("FileType", {
@@ -352,3 +419,18 @@ vim.g.coc_global_extensions = {
   'coc-sumneko-lua',
   'coc-sh',
 }
+
+-- Normal = { bg = "" },
+-- SignColumn = { bg = "" },
+-- WinSeparator = { fg = ayu.ui, bg = "" },
+-- Visual = { bg = ayu.selection_border },
+-- CursorLine = { bg = lightBlue },
+-- CursorLineNr = { bg = lightBlue },
+-- Search = { bg = ayu.ui },
+-- --Comment = gray,
+-- LineNr = { fg = ayu.comment },
+-- NonText = gray,
+-- DiagnosticHint = gray,
+-- SpecialKey = gray,
+-- NvimTreeIndentMarker = gray,
+-- Folded = { fg = 'lightgray', bg = "" },
